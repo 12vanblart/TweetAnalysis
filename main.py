167febe13f2bbb652cbf48e8157617685e3e65e5
12vanblart <12vanblart@gmail.com>
2019-01-30 11:26:53 -0500

    Starting to work on main()

---
+++ main.py
@@ -0,0 +1,20 @@
+####################
+## 12vanblart ######
+## Tweet Analysis ##
+####################
+
+
+def main():
+    print("--------------------------------------------")
+    print("Tweet Anlaysis Time!")
+    print("--------------------------------------------")
+    print("First things first!")
+    print("Unzip your twitter export and rename the folder to to `tweets` ")
+    print("Then place the folder in the same location as this program")
+    input("Press Enter once completed")
+    totalTweets = input(
+        "How many total tweets does your profile show (number, no commas)? ")
+
+
+if __name__ == "__main__":
+    main()
