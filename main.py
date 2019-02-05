5e13d9a5bcc96b1ae9aa0250bbe4c7e9144d0c6c
Tyler <12vanblart@gmail.com>
2019-02-05 02:06:49 +0000

    Added Regex checks, counter, and finalized output

--- main.py
+++ main.py
@@ -3,18 +3,18 @@
 ## Tweet Analysis ##
 ####################
 import csv
+from collections import Counter
+import re
 
 
 def main():
     remainingTweets = 0
     replyTweets = 0
-    androidTweets = 0
-    webTweets = 0
-    youtubeLikeTweets = 0
     reTweets = 0
     hashtaggedTweets = 0
     mentionTweets = 0
     imageTweets = 0
+    interfaceCollection = []
     print("--------------------------------------------")
     print("Tweet Anlaysis Time!")
     print("--------------------------------------------")
@@ -34,6 +34,34 @@ def main():
             remainingTweets += 1
             if row[1] != "":
                 replyTweets += 1
+            if row[6] != "":
+                reTweets += 1
+            try:
+                found = re.search('">(.+?)</a', row[4]).group(1)
+            except:
+                found = ''
+            interfaceCollection.append(found)
+            try:
+                foundAt = re.search('@', row[5])
+                if foundAt:
+                    mentionTweets += 1
+            except:
+                found = ''
+            try:
+                foundHashtag = re.search('#', row[5])
+                if foundHashtag:
+                    hashtaggedTweets += 1
+            except:
+                found = ''
+            try:
+                foundImage = re.search('/photo/', row[9])
+                if foundImage:
+                    imageTweets += 1
+            except:
+                found = ''
+
+    # Count values from list
+    commonInterface = Counter(interfaceCollection).most_common()
     print("--------------------------------------------")
     print("--------------------------------------------")
     print("--------------------------------------------")
@@ -47,9 +75,27 @@ def main():
     print("This translates to", "{0:.2f}".format(
         (100 * replyTweets/totalTweets)), "% of your total tweets")
     print("###############")
-    print("Your tweets were ", replyTweets, " of your tweets")
+    print("You retweeted", reTweets, "tweets")
     print("This translates to", "{0:.2f}".format(
-        (100 * replyTweets/totalTweets)), "% of your total tweets")
+        (100 * reTweets/totalTweets)), "% of your total tweets")
+    print("###############")
+    print("You mentioned somone in", mentionTweets, "of your tweets")
+    print("This translates to", "{0:.2f}".format(
+        (100 * mentionTweets/totalTweets)), "% of your total tweets")
+    print("###############")
+    print("You used at least one hashtag in",
+          hashtaggedTweets, "of your tweets")
+    print("This translates to", "{0:.2f}".format(
+        (100 * hashtaggedTweets/totalTweets)), "% of your total tweets")
+    print("###############")
+    print("Images appeared in", imageTweets, "of your tweets")
+    print("This translates to", "{0:.2f}".format(
+        (100 * imageTweets/totalTweets)), "% of your total tweets")
+    print("###############")
+    print("Your tweet breakdown by platform of origin:")
+    for i in range(len(commonInterface)):
+        print(str(i+1), ": ", str(commonInterface[i][0]).strip(), ": ", "{0:.2f}".format(
+            (100*commonInterface[i][1]/totalTweets)), "%", sep="")
 
     csv_file.close()
 
